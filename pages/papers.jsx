01c5636f2b12c06b735bb3f38002ff0d0f54a00a
aaditya9803 <aaditya9803@gmail.com>
2025-03-24 17:48:59 +0100

    reports added

--- pages/papers.jsx
+++ pages/papers.jsx
@@ -11,10 +11,10 @@ const PapersPage = ({ papers }) => {
           <div key="a" className={styles.card}>
             <div className={styles.content}>
               <h3>A Short Report On Predictive Models for Condition Monitoring of Hydraulic Systems</h3>
-              <p><strong>Abstract:</strong> About the project</p>
+              <p><strong>Abstract:</strong> Report on machine learning models for Predictive Maintainance of Hydraulic Systems</p>
               <div className={styles.cta}>
                 <a
-                  href="https://google.com"
+                  href="/short_report.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   className={styles.underline}
@@ -28,11 +28,17 @@ const PapersPage = ({ papers }) => {
 
           <div key="a" className={styles.card}>
             <div className={styles.content}>
-              <h3>A Short Report On Predictive Models for Condition Monitoring of Hydraulic Systems</h3>
-              <p><strong>Abstract:</strong> About the project</p>
+              <h3>A paper on Rainwater Harvesting System on Urban Areas for
+              Sustainability (Uni Assignment)</h3>
+              <p><strong>Abstract:</strong> This study assesses the
+                  suitability of Rainwater Harvesting with the focus on Roof Top
+                  Rainwater Harvesting model in urban areas. This paper gives an
+                  overview of RWHS that can be harvested during monsoon and
+                  gives the analyzed and interpreted framework of calculation of
+                  catchment area, discharge, and tank design.</p>
               <div className={styles.cta}>
                 <a
-                  href="https://google.com"
+                  href="/scientific_paper.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   className={styles.underline}
