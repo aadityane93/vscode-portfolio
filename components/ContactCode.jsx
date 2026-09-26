7791d689339f039f50245e5c77d6109cf2689995
aadityane93 <ne.neupane@gmail.com>
2026-09-26 15:11:25 +0200

    issue: contact page, lines not fitting fixed

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -9,16 +9,18 @@ const contactItems = [
   {
     social: 'LinkedIn',
     link: 'linkedin.com/in/aaditya-neupane-944523287',
+    mobileLink: 'aaditya-neupane-944523287',
     href: 'https://www.linkedin.com/in/aaditya-neupane-944523287/',
   },
   {
     social: 'GitHub',
-    link: 'github.com/aadityane93',
+    link: 'aadityane93',
     href: 'https://github.com/aadityane93',
   },
   {
     social: 'Kaggle',
     link: 'kaggle.com/neupaneaaditya',
+    mobileLink: 'neupaneaaditya',
     href: 'https://kaggle.com/neupaneaaditya/code',
   },
   {
@@ -37,22 +39,24 @@ const ContactCode = () => {
       <p className={styles.line}>
         <span>Aaditya</span>&#58;
       </p>
-      <p className={styles.line}>
-        &nbsp;&nbsp;&nbsp;&#8212; <span>socials</span>&#58;
+      <p className={`${styles.line} ${styles.socialsHeader}`}>
+        <span className={styles.indent}>&nbsp;&nbsp;&nbsp;</span>&#8212; <span>socials</span>&#58;
       </p>
       {contactItems.slice(0, 8).map((item, index) => (
-        <p className={styles.line} key={index}>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{' '}
+        <p className={`${styles.line} ${styles.socialLine}`} key={index}>
+          <span className={styles.indent}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>{item.social}:{' '}
           <a href={item.href} target="_blank" rel="noopener">
-            {item.link}
+            <span className={styles.desktopLink}>{item.link}</span>
+            <span className={styles.mobileLink}>{item.mobileLink || item.link}</span>
           </a>
         </p>
       ))}
       {contactItems.slice(8, contactItems.length).map((item, index) => (
-        <p className={styles.line} key={index}>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{' '}
+        <p className={`${styles.line} ${styles.socialLine}`} key={index}>
+          <span className={styles.indent}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>{item.social}:{' '}
           <a href={item.href} target="_blank" rel="noopener">
-            {item.link}
+            <span className={styles.desktopLink}>{item.link}</span>
+            <span className={styles.mobileLink}>{item.mobileLink || item.link}</span>
           </a>
         </p>
       ))}
